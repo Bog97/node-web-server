246ecb2cb52ffdf8938598739f55edc40a6d06ed
Tudos <Bogdan.Tudos1@T-Mobile.com>
2018-07-16 15:31:01 -0700

    Set up start script and set up Heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');  // HANDLEBARS
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 let app = express();
 
@@ -21,9 +22,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs')
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs')
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -61,6 +62,6 @@ app.get('/bad', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
